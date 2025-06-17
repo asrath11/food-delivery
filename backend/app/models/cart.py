73b319f6d5461837a60d05201d3d4b64884e0ca5
Asrath <pasrath.2004@gmail.com>
2025-06-17 16:06:35 +0530

    added cart model

---
+++ backend/app/models/cart.py
@@ -0,0 +1,18 @@
+from app.models.db import db
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+import uuid
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+
+
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(String(100), ForeignKey('users.id'), nullable=False)
+    item_id = Column(String(100), ForeignKey('items.id'), nullable=False)
+    quantity = Column(Integer, nullable=False, default=1)
+
+    # Relationships
+    user = relationship("User", backref="cart_items", lazy=True)
+    item = relationship("Item", backref="in_carts", lazy=True)
