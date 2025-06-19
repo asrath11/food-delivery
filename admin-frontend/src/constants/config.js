de613ec8b8934f5b0ca60dc0011bb869ea01af42
Asrath <pasrath.2004@gmail.com>
2025-06-19 14:49:29 +0530

    change cart url

--- admin-frontend/src/constants/config.js
+++ admin-frontend/src/constants/config.js
@@ -1,3 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const LOGIN_URL = `${API_URL}/auth/login`;
 export const SIGNUP_URL = `${API_URL}/auth/signup`;
+export const ITEMS_URL = `${API_URL}/items/`;
+export const CART_URL = `${API_URL}/cart/`;
