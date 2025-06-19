4e4da0ab773bd4e39ce6aa924495c846fa01e092
Asrath <pasrath.2004@gmail.com>
2025-06-19 14:47:49 +0530

    added cart provider

--- admin-frontend/src/components/Layout.jsx
+++ admin-frontend/src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from './navigation/Navigation';
+import AppProviders from '../hooks/AppProviders';
 
 function Layout() {
   return (
-    <>
+    <AppProviders>
       <Navigation></Navigation>
       <Outlet />
-    </>
+    </AppProviders>
   );
 }
 
