f22679f0d078e9f62099aab33801e2aa60f3b224
Asrath <pasrath.2004@gmail.com>
2025-06-21 16:05:45 +0530

    added AddItems component

--- admin-frontend/src/pages/AdminDashBoard.jsx
+++ admin-frontend/src/pages/AdminDashBoard.jsx
@@ -1,26 +1,28 @@
-import React,{useState} from 'react';
-import {
-  List,
-  Trash2,
-  Pencil,
-  X,
-  LogOut,
-} from 'lucide-react'; // Import only what you need
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { List, Trash2, LogOut } from 'lucide-react'; // Import only what you need
 import { Button } from '@/components/ui/button';
+import AddItems from '@/components/admin-dashboard/AddItems';
 
 const menuItems = [
-  { id: 'addItems', label: 'Add Items', icon: List },
-  { id: 'removeItems', label: 'Remove Items', icon: Trash2 },
+  { id: 'addItems', label: 'Add Items', icon: List, component: <AddItems /> },
+  {
+    id: 'removeItems',
+    label: 'Remove Items',
+    icon: Trash2,
+    // component: <RemoveItems />,
+  },
   { id: 'logout', label: 'Logout', icon: LogOut },
 ];
 function AdminDashBoard() {
-  const [activeTab,setActiveTab]=useState("addItems")
+  const [activeTab, setActiveTab] = useState('addItems');
   return (
-    <div className="flex h-screen w-full">
+    <div className='flex h-screen w-full'>
       {/* Sidebar (Dark, with primary accent) */}
-      <div className="w-1/5 h-full p-4 text-black shadow-lg">
-        <h2 className="text-2xl font-bold mb-6 text-black">Admin Panel</h2> {/* Primary color */}
-        <ul className="space-y-3">
+      <div className='w-1/5 h-full p-4 text-black shadow-lg'>
+        <h2 className='text-2xl font-bold mb-6 text-black'>Admin Panel</h2>{' '}
+        {/* Primary color */}
+        <ul className='space-y-3'>
           {menuItems.map((item) => (
             <li
               key={item.id}
@@ -29,7 +31,7 @@ function AdminDashBoard() {
               }`}
               onClick={() => setActiveTab(item.id)}
             >
-              <item.icon className="size-5" /> {/* Primary light */}
+              <item.icon className='size-5' /> {/* Primary light */}
               <span>{item.label}</span>
             </li>
           ))}
@@ -37,13 +39,8 @@ function AdminDashBoard() {
       </div>
 
       {/* Main Content (Light) */}
-      <div className="w-4/5 h-full p-6 overflow-y-auto">
-        <h1 className="text-3xl font-semibold mb-4">Welcome to Admin Dashboard</h1>
-        <p className="text-[#3A3A3A]/80">Manage your food delivery system here.</p>
-        {/* Example button with primary color */}
-        <Button className="mt-4 px-4 py-2 rounded bg-brand hover:bg-brand text-white">
-          Save Changes
-        </Button>
+      <div className='w-4/5 h-full p-6 overflow-y-auto'>
+        {menuItems.map((item) => activeTab === item.id && item.component)}
       </div>
     </div>
   );
