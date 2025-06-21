6f830f63c3fcc455797ae48962a9b4fd91e1a89b
Asrath <pasrath.2004@gmail.com>
2025-06-21 16:16:00 +0530

    added use debounce hook

--- admin-frontend/src/hooks/CartProvider.jsx
+++ admin-frontend/src/hooks/CartProvider.jsx
@@ -12,58 +12,45 @@ const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartData, setCartData] = useState({ cart: [], total: 0 });
-  const [isLoading, setIsLoading] = useState(false);
   const { user, isLoading: userLoading } = useUser();
 
-  const addToCart = async (item) => {
+  const addToCart = async (item_id) => {
     try {
-      setIsLoading(true);
-      await addItemToCart(item.id);
+      await addItemToCart(item_id);
       const updatedCart = await getItemsInCart();
       setCartData(updatedCart);
     } catch (err) {
       console.error('Add to cart error:', err.response?.data || err.message);
-    } finally {
-      setIsLoading(false);
     }
   };
 
   const updateQuantity = async (cart_item_id, quantity) => {
     try {
-      setIsLoading(true);
       await updateCartItemQuantity(cart_item_id, quantity);
-    } catch (err) {
-      console.error('Update quantity error:', err.response?.data || err.message);
-    } finally {
       const updatedCart = await getItemsInCart();
       setCartData(updatedCart);
-      setIsLoading(false);
+    } catch (err) {
+      console.error('Update quantity error:', err.response?.data || err.message);
     }
   };
 
   const removeFromCart = async (cart_item_id) => {
     try {
-      setIsLoading(true);
       await removeCartItemFromCart(cart_item_id);
-    } catch (err) {
-      console.error('Remove from cart error:', err.response?.data || err.message);
-    } finally {
       const updatedCart = await getItemsInCart();
       setCartData(updatedCart);
-      setIsLoading(false);
+    } catch (err) {
+      console.error('Remove from cart error:', err.response?.data || err.message);
     }
   };
 
   useEffect(() => {
     const fetchCart = async () => {
       try {
-        setIsLoading(true);
         const res = await getItemsInCart();
         setCartData(res);
-      } catch (error) {
-        console.error('Failed to fetch cart:', error);
-      } finally {
-        setIsLoading(false);
+      } catch (err) {
+        console.error('Fetch cart error:', err.response?.data || err.message);
       }
     };
 
@@ -76,7 +63,7 @@ export const CartProvider = ({ children }) => {
     <CartContext.Provider
       value={{
         cartData,
-        isLoading,
+        setCartData,
         addToCart,
         updateQuantity,
         removeFromCart,
@@ -88,10 +75,4 @@ export const CartProvider = ({ children }) => {
 };
 
 // Custom hook to use cart context
-export const useCart = () => {
-  const context = useContext(CartContext);
-  if (context === undefined) {
-    throw new Error('useCart must be used within a CartProvider');
-  }
-  return context;
-};
+export const useCart = () => useContext(CartContext);
