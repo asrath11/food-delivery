f0ce8409577d487a54a4c4eb37f6df976461f8d6
Asrath <pasrath.2004@gmail.com>
2025-06-25 22:17:51 +0530

    added requirement.txt

--- backend/main.py
+++ backend/main.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     print("Production server started at http://127.0.0.1:5000")
-    serve(app, host="127.0.0.1", port=5000)
+    serve(app, host="127.0.0.1", port=5000, threads=6)
