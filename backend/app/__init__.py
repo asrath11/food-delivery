27c9c7b13250c774393d688d1d2971a86fe6a866
Asrath <pasrath.2004@gmail.com>
2025-06-26 11:18:35 +0530

    added frontend production url

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -46,7 +46,7 @@ def create_app():
     app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024  # 10 MB
     CORS(app,
      supports_credentials=True,
-     origins=["https://food-delivery-npm1.onrender.com"],
+     origins=["https://food-delivery-admin-45xn.onrender.com"],
      allow_headers=["Content-Type", "Authorization"],
      methods=["GET", "POST", "DELETE", "PUT","PATCH", "OPTIONS"])
     db.init_app(app)
